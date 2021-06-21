3b42675a524ecb468c7cc76f88a4e318f1307f54
kentina <49018946+kentina@users.noreply.github.com>
2021-06-21 17:52:08 +0700

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,12 @@
 <template>
   <header class="sticky top-0 z-50 border-b bg-white dark:bg-black dark:border-gray-darkest">
     <div class="flex items-center justify-between container py-2 sm:py-4">
-      <div class="flex flex-nowrap items-center space-x-4 py-2 sm:py-0">
+      <div class="flex flex-nowrap items-center space-x-4 py-2 sm:py-0">        
+        <img
+          :alt="title"
+          src="/logo.svg"
+          class="w-5" />
         <a href="/">
-          <img
-            :alt="title"
-            src="/logo.svg"
-            class="w-5" />
           <h1 class="font-bold text-xl whitespace-nowrap sm:text-2xl">
             {{ title }}
           </h1>
